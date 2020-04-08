2480d6466d013dd9c2b2fbc8cecaabab82b6c140
index325 <gabriel.moraes.o@hotmail.com>
2020-04-07 22:49:55 -0300

    initial route - /api

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,5 +17,11 @@ routes.post('/tipo_entrega', TipoDeEntregaController.guardar);
 routes.post('/cliente', ClienteController.guardar);
 routes.post('/pedido', PedidosController.guardar);
 
+routes.get('/', function (req, res, next) {
+    res.status(200).send({
+        title: "Node Express API",
+        version: "0.0.1"
+    });
+});
 
 export default routes;
